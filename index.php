285ef59df6450c62974c2346f1e9a78e15656bb1
Deepak <deepakchoudharysl78@gmail.com>
2023-05-26 15:03:11 +0530

    New website

---
+++ index.php
@@ -0,0 +1,470 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome To Official Website Of Appolo hospital </title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<header class="header">
+
+    <a href="#" class="logo"> <i class="fas fa-heartbeat"></i> <strong>APPOLO</strong>  HOSPITAL </a>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="#services">services</a>
+        <a href="#doctors">doctors</a>
+        <a href="#review">review</a>
+        <a href="#blogs">blogs</a>
+        <a href="#faq">FaQ</a>
+        <a href="#footer">Contact Us</a>
+    </nav>
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+</header>
+
+<section class="home" id="home">
+
+    <div class="image">
+        <img src="image/home-img.svg" alt="">
+    </div>
+
+    <div class="content">
+        <h3>we take care of your healthy life</h3>
+        <p> A person who has good physical health is likely to have bodily functions and processes working at their peak.</p>
+        <a href="#footer" class="btn"> Contact Us <span class="fas fa-chevron-right"></span> </a>
+    </div>
+
+</section>
+
+<section class="icons-container">
+
+    <div class="icons">
+        <i class="fas fa-user-md"></i>
+        <h3>25+</h3>
+        <p>doctors at work</p>
+    </div>
+
+    <div class="icons">
+        <i class="fas fa-procedures"></i>
+        <h3>200+</h3>
+        <p>bed facility</p>
+    </div>
+
+    <div class="icons">
+        <i class="fas fa-user-nurse"></i>
+        <h3>50+</h3>
+        <p>Staffs at work</p>
+    </div>
+
+    <div class="icons">
+        <i class="fas fa-ambulance"></i>
+        <h3>20+</h3>
+        <p>ambulances</p>
+    </div>
+
+</section>
+
+<section class="about" id="about">
+
+    <h1 class="heading"> <span>about</span> us </h1>
+
+    <div class="row">
+
+        <div class="image">
+            <img src="image/about-img.svg" alt="">
+        </div>
+
+        <div class="content">
+            <h3>take the world's best quality treatment</h3>
+            <p>We have the best doctors and the most care-focused teams coupled with the most advanced equipment, World-Class Specilist and more. These ensure we can get you healthy quickly so you can get back to leading a happy and fulfilling life.</p>
+            <a href="#services" class="btn"> Our services <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="services" id="services">
+
+    <h1 class="heading"> our <span>services</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <i class="fas fa-notes-medical"></i>
+            <h3>checkups</h3>
+            <p>We have world-class checkup standard.</p>
+            <a href="#appointment" class="btn"> book appointment <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-ambulance"></i>
+            <h3>24/7 ambulance</h3>
+            <p>our ambulance are supported 24 hours a day 7 days in a week.</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-user-md"></i>
+            <h3>expert doctors</h3>
+            <p>we have world-class specialized team of doctors.</p>
+            <a href="#doctors" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-pills"></i>
+            <h3>medicines</h3>
+            <p>we have collaborated with Netmeds , which provides IMA approved medicines.</p>
+            <a href="https://www.netmeds.com/order-medicine" class="btn"> buy now <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-procedures"></i>
+            <h3>bed facility</h3>
+            <p>we have 200+ beds with best medical equipment at a affordable price.</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-heartbeat"></i>
+            <h3>total care</h3>
+            <p>We provide all kind of Medical Support.</p>
+        </div>
+
+        <div class="box">
+            <i class="fas fa-flask"></i>
+            <h3>lab test</h3>
+            <p>we have world-class laboratory but we can accept netmeds lab test reports also.</p>
+            <a href="https://www.netmeds.com/health-packages" class="btn"> book lab test <span class="fas fa-chevron-right"></span> </a>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="doctors" id="doctors">
+
+    <h1 class="heading"> our <span>doctors</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="image/doc-1.jpg" alt="">
+            <h3>Dr. Rajani Sinha</h3>
+            <span>General Physician</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+                
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/doc-2.jpg" alt="">
+            <h3>Dr. Raju Kumar</h3>
+            <span>appendix specialist</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="https://www.instagram.com/manasjha9809/" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/doc-3.jpg" alt="">
+            <h3>Dr. Sangeeta Ratna</h3>
+            <span>Homoeopath</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/doc-4.jpg" alt="">
+            <h3>Dr. Neena Agarwal</h3>
+            <span>Gynecologist/Obstetrician</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/doc-5.jpg" alt="">
+            <h3>Dr. Hemant Kumar</h3>
+            <span>Psychiatrist</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+        <div class="box">
+            <img src="image/doc-6.jpg" alt="">
+            <h3>Dr. Sujit Jha</h3>
+            <span>Sexologist</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            
+            </div>
+        </div>
+        <div class="box">
+            <img src="image/doc-7.jpg" alt="">
+            <h3>Dr. Rashmi Prasad</h3>
+            <span>Gynecologist/Obstetrician</span>
+            <div class="share">
+                
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="image/doc-8.jpg" alt="">
+            <h3>Dr. Vinay Kumar</h3>
+            <span>Ayurveda</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+        <div class="box">
+            <img src="image/doc-9.jpg" alt="">
+            <h3>Dr. Sanjay Kumar</h3>
+            <span>Pulmonologist</span>
+            <div class="share">
+            <a href="#" class="fab fa-facebook-f"></a>
+                <a href="#" class="fab fa-twitter"></a>
+                <a href="#" class="fab fa-instagram"></a>
+                <a href="#" class="fab fa-linkedin"></a>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="review" id="review">
+    
+    <h1 class="heading"> client's <span>review</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <img src="image/pic-1.jpg" alt="">
+            <h3>Om Niwas</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+            </div>
+            <p class="text">Good doctor. He is very simple and gentle in his behavior. He will give sufficient time to each patient.</p>
+        </div>
+
+        <div class="box">
+            <img src="image/pic-1.jpg" alt="">
+            <h3>Dipak Kumar</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fa-regular fa-star"></i>
+            </div>
+            <p class="text">Nice place very clear and staffs are very supportive but Fees is very high.</p>
+        </div>
+
+        <div class="box">
+            <img src="image/pic-1.jpg" alt="">
+            <h3>Deepak Choudhary</h3>
+            <div class="stars">
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star-half-alt"></i>
+            </div>
+            <p class="text">Very good doctor and kind at heart. Gives maximum time to hear patient views.</p>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="blogs" id="blogs">
+
+    <h1 class="heading"> our <span>blogs</span> </h1>
+
+    <div class="box-container">
+
+        <div class="box">
+            <div class="image">
+                <img src="image/blog-1.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a> <i class="fas fa-calendar"></i> 4 January, 2023 </a>
+                    <a> <i class="fas fa-user"></i> by Appolo </a>
+                </div>
+                <h3>Bihar Corona: भागलपुर में कोरोना के आधे दर्जन मरीज मिले, 231 लोगों में कोविड के लक्षण, जानिए ताजा हालात</h3>
+                <a href="https://www.prabhatkhabar.com/state/bihar/bhagalpur/bhagalpur-corona-cases-today-update-as-doctor-and-6-corona-positive-in-bhagalpur-bihar-covid-news-skt" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="image/blog-2.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a> <i class="fas fa-calendar"></i> 4 January, 2023 </a>
+                    <a> <i class="fas fa-user"></i> by Appolo </a>
+                </div>
+                <h3>Good News: मुफ्त में यह कोर्स कर बनें आंखों का डॉक्टर, यह संस्था दे रही है मौका, जानिए डिटेल्स</h3>
+                <a href="https://hindi.news18.com/news/bihar/purnia-become-an-eye-doctor-by-doing-bachelor-of-optometry-course-for-free-5425841.html" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+
+        <div class="box">
+            <div class="image">
+                <img src="image/blog-3.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="icon">
+                    <a> <i class="fas fa-calendar"></i> 4 January, 2023 </a>
+                    <a> <i class="fas fa-user"></i> by Appolo </a>
+                </div>
+                <h3>Rishabh Pant Health Update: Good news</h3>
+                <a href="https://www.insidesport.in/rishabh-pant-health-update-good-news-rishabh-pant-finally-back-home-after-a-month-in-hospital-posts-heartfelt-message-for-fans-check-out/" class="btn"> learn more <span class="fas fa-chevron-right"></span> </a>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section class="blogs" id="faq">
+
+    <h1 class="heading"> Frequently <span> Asked Questions </span> </h1>
+
+    <div class="box-container">
+        <div class="box">
+            <div class="content">
+                <div class="icon">
+                    <a> <i class="fas fa-calendar"></i> 7 January, 2023 </a>
+                    <a> <i class="fas fa-user"></i> By Expert Doctor </a>
+                </div>
+                <div class="faqs">
+
+                    <h3>
+                        Q1).What are the conditions treated by general physicians?
+                    </h3>
+                    <p>
+                        Ans: General physicians treat conditions like headaches, flu, urinary infections, blood pressure, diabetes, common aches, etc.
+                    </p>
+
+                    <h3>
+                        Q2).Do general physicians offer home consultation?
+                    </h3>
+                    <p>
+                        Ans: Yes, most of the general physicians do offer home consultation in case of emergencies or when requested.
+                    </p>
+
+                    <h3>
+                        Q3).Will the consultation and diagnosis be covered in insurance?
+                    </h3>
+                    <p>
+                        Ans: Most of the conditions treated by general physicians are common. Hence, most of them are not covered by insurance. However, you can always check with your insurance company.
+                    </p>
+
+                    <h3>
+                        Q4).Do general physicians provide in-house medicines?
+                    </h3>
+                    <p>
+                        Ans: Yes, some of the general physicians do provide in-house medicines along with prescribing medication.
+                    </p>
+
+                    <h3>
+                        Q5).What are the consultation charges of general physicians in Patna?
+                    </h3>
+                    <p>
+                        Ans: The consultation fees of general physicians in Patna ranges approximately from Rs. 250 to Rs. 500.
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+<section class="footer" id="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#home"> <i class="fas fa-chevron-right"></i> home </a>
+            <a href="#about"> <i class="fas fa-chevron-right"></i> about </a>
+            <a href="#services"> <i class="fas fa-chevron-right"></i> services </a>
+            <a href="#doctors"> <i class="fas fa-chevron-right"></i> doctors </a>
+            <a href="#review"> <i class="fas fa-chevron-right"></i> review </a>
+            <a href="#blogs"> <i class="fas fa-chevron-right"></i> blogs </a>
+        </div>
+
+        <div class="box">
+            <h3>our services</h3>
+            <a href="#services"> <i class="fas fa-chevron-right"></i> dental care </a>
+            <a href="#services"> <i class="fas fa-chevron-right"></i> message therapy </a>
+            <a href="#services"> <i class="fas fa-chevron-right"></i> cardioloty </a>
+            <a href="#services"> <i class="fas fa-chevron-right"></i> diagnosis </a>
+            <a href="#services"> <i class="fas fa-chevron-right"></i> ambulance service </a>
+        </div>
+
+        <div class="box">
+            <h3>appointment info</h3>
+            <a> <i class="fas fa-phone"></i> +91-1234567890 </a>
+            <a> <i class="fas fa-phone"></i> +91-1234567890 </a>
+            <a> <i class="fas fa-envelope"></i> sales@appolo.com </a>
+            <a> <i class="fas fa-envelope"></i> contact@appolo.com </a>
+            <a> <i class="fas fa-map-marker-alt"></i> Patna, Bihar </a>
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="https://twitter.com"> <i class="fab fa-twitter"></i> twitter </a>
+            <a href="https://instagram.com"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="https://linkedin.com"> <i class="fab fa-linkedin"></i> linkedin </a>
+            <a href="https://pinterest.com"> <i class="fab fa-pinterest"></i> pinterest </a>
+        </div>
+
+    </div>
+    <div class="credit"> created by <span>OM NIWAS, DIPAK KUMAR & DEEPAK CHOUDHARY</span> | all rights reserved </div>
+
+</section>
+<script src="js/script.js"></script>
+
+</body>
+</html>
